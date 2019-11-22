69b98b8e0a548fbad04a51dcf25baf2b79abefe9
Yumibyte <ashraigosa@gmail.com>
2019-11-21 22:23:49 -0800

    Embedded in navigation controller w/ tableView labeled mySongs. Initailization of Firebase

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+
+target 'TranslationApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TranslationApp
+pod 'Firebase'
+pod 'Firebase/Auth'
+pod 'Firebase/Database'
+end
---
+++ TranslationApp/MySongs.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TranslationApp
+//
+//  Created by Ashley Raigosa on 11/21/19.
+//  Copyright © 2019 Ashley Raigosa. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
