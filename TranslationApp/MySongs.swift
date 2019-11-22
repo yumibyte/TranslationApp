92949d0a2d1ad607706bce76e27bbd563c845a29
Yumibyte <ashraigosa@gmail.com>
2019-11-21 22:49:38 -0800

    began adding dequeue reusable cell. Need to add more options for cell in code

--- TranslationApp/MySongs.swift
+++ TranslationApp/MySongs.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
-import Firebase
+//import Firebase
 
 class ViewController: UITableViewController {
-
+    
+    var songArray: [Song] = [Song]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customSongCell", for: indexPath)
+        
+        
+        return cell
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return songArray?.count ?? 1
+    }
 
 }
 
---
+++ TranslationApp/Song.swift
@@ -0,0 +1,14 @@
+//
+//  Song.swift
+//  TranslationApp
+//
+//  Created by Ashley Raigosa on 11/21/19.
+//  Copyright © 2019 Ashley Raigosa. All rights reserved.
+//
+
+import Foundation
+
+class Song {
+    var title: String = ""
+    var singer: String = ""
+}
